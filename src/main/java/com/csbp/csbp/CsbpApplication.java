749d239ea8d9a005e0d08bebc31ce625370cadae
santihoyos15 <santiago@digitalmedic.co>
2024-09-08 15:14:12 -0500

    Initialize project

---
+++ src/main/java/com/csbp/csbp/CsbpApplication.java
@@ -0,0 +1,13 @@
+package com.csbp.csbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csbp/csbp/CsbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csbp.csbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
